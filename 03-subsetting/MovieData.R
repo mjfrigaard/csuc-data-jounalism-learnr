ee5aebb18c8db1c26be85277d1ab88005d6df7f6
Martin J Frigaard <mjfrigaard@gmail.com>
2021-11-13 21:18:12 -0700

    funs

--- 03-subsetting/MovieData.R
+++ 03-subsetting/MovieData.R
@@ -1,28 +1,35 @@
 library(ggplot2movies)
 library(tidyverse)
 
+create_movie_data <- function() {
+  movies <- ggplot2movies::movies
+    MovieData <- movies %>% 
+        dplyr::filter(year > 2000 & mpaa != "") %>% 
+        tidyr::pivot_longer(cols = c(Action:Short), 
+                     names_to = "genre_key", 
+                     values_to = "genre_value") %>% 
+        dplyr::select(title:rating, mpaa, genre_key, genre_value) %>% 
+        dplyr::filter(genre_value == 1) %>% 
+        dplyr::mutate(
+            genre_value = case_when(
+                genre_key == 'Action' ~ "action",
+                genre_key == 'Animation' ~ "animation",
+                genre_key == 'Comedy' ~ "comedy",
+                genre_key == 'Drama' ~ "drama",
+                genre_key == 'Documentary' ~ "documentary",
+                genre_key == 'Romance' ~ "romance",
+                genre_key == 'Short' ~ "short",
+                TRUE ~ NA_character_)) %>% 
+        tidyr::pivot_wider(names_from = genre_key, 
+                    values_from = genre_value) %>% 
+        tidyr::unite(col = "genres", 
+                     Action:Short, 
+                     sep = ", ") %>% 
+        dplyr::mutate(
+            genres = str_remove_all(string = genres, pattern = "NA, "),
+            genres = str_remove_all(string = genres, pattern = ", NA"))
+    return(MovieData)
+}
 
-movies <- ggplot2movies::movies
-MovieData <- movies %>% 
-    filter(year > 2000 & mpaa != "") %>% 
-    pivot_longer(cols = c(Action:Short), 
-                 names_to = "genre_key", 
-                 values_to = "genre_value") %>% 
-    select(title:rating, mpaa, genre_key, genre_value) %>% 
-    filter(genre_value == 1) %>% 
-    mutate(genre_value = case_when(
-        genre_key == 'Action' ~ "action",
-        genre_key == 'Animation' ~ "animation",
-        genre_key == 'Comedy' ~ "comedy",
-        genre_key == 'Drama' ~ "drama",
-        genre_key == 'Documentary' ~ "documentary",
-        genre_key == 'Romance' ~ "romance",
-        genre_key == 'Short' ~ "short",
-        TRUE ~ NA_character_)) %>% 
-    pivot_wider(names_from = genre_key, 
-                values_from = genre_value) %>% 
-    unite(col = "genres", Action:Short, sep = ", ") %>% 
-    mutate(genres = str_remove_all(string = genres, pattern = "NA, "),
-           genres = str_remove_all(string = genres, pattern = ", NA"))
 # export
 # write_csv(x = MovieData, file = "03-subsetting/MovieData.csv")
